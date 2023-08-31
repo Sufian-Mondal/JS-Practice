58cdfe68e2a6e008f2fa2ce65ce1db9da37a2f28
Abu <sufianmondal.eee@gmail.com>
2023-08-31 12:33:50 +0600

    Dom basic start

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 // gussing game
 
-var numberOfWins = 0;
-var numberOfLosts = 0;
+// var numberOfWins = 0;
+// var numberOfLosts = 0;
 
-for (i = 1; i <= 10; i++) {
-  var guessNumber = parseInt(prompt("Enter a random number from 1 to 10 : "));
-  var randomNumber = Math.floor(Math.random() * 10) + 1;
+// for (i = 1; i <= 10; i++) {
+//   var guessNumber = parseInt(prompt("Enter a random number from 1 to 10 : "));
+//   var randomNumber = Math.floor(Math.random() * 10) + 1;
 
-  if (guessNumber == randomNumber) {
-    console.log("You have won");
-    numberOfWins++;
-  } else {
-    console.log("You have lost. Random number was" + randomNumber);
-    numberOfLosts++;
-  }
-}
+//   if (guessNumber == randomNumber) {
+//     console.log("You have won");
+//     numberOfWins++;
+//   } else {
+//     console.log("You have lost. Random number was" + randomNumber);
+//     numberOfLosts++;
+//   }
+// }
 
-document.write("Total number of wins = " + numberOfWins + "<br>");
-document.write("Total number of Losts = " + numberOfLosts);
+// document.write("Total number of wins = " + numberOfWins + "<br>");
+// document.write("Total number of Losts = " + numberOfLosts);
+
+var myHeading = document.getElementById("heading1");
+myHeading.innerHTML = "Hello, I am sufian";
+
+var myHeading2 = document.getElementById("heading2");
+myHeading2.innerHTML = "Goodbye goodbye";
+
+var myPera = document.getElementById("pera1");
+myPera.innerHTML = "Today is thursday";
