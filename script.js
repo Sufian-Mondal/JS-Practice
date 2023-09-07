fa0766d03e5c7c1e7245ac5146ec09269049aeab
Abu <sufianmondal.eee@gmail.com>
2023-09-07 12:25:01 +0600

    add css style dynamicaly using js

--- script.js
+++ script.js
@@ -28,34 +28,44 @@
 // var myPera = document.getElementById("pera1");
 // myPera.innerHTML = "Today is thursday";
 
-var photos = [
-  "images/sufian-1.png",
-  "images/sufian-2.png",
-  "images/sufian-3.png",
-];
+// var photos = [
+//   "images/sufian-1.png",
+//   "images/sufian-2.png",
+//   "images/sufian-3.png",
+// ];
 
-var imgTag = document.querySelector("img");
+// var imgTag = document.querySelector("img");
 
-var count = 0;
+// var count = 0;
 
-function next() {
-  count++;
+// function next() {
+//   count++;
 
-  if (count >= photos.length) {
-    count = 0;
-    imgTag.src = photos[count];
-  } else {
-    imgTag.src = photos[count];
-  }
+//   if (count >= photos.length) {
+//     count = 0;
+//     imgTag.src = photos[count];
+//   } else {
+//     imgTag.src = photos[count];
+//   }
+// }
+
+// function prev() {
+//   count--;
+
+//   if (count < 0) {
+//     count = photos.length - 1;
+//     imgTag.src = photos[count];
+//   } else {
+//     imgTag.src = photos[count];
+//   }
+// }
+
+var myVar = document.querySelector("#paraId");
+
+function addStyle() {
+  myVar.classList.add("para-style");
 }
 
-function prev() {
-  count--;
-
-  if (count < 0) {
-    count = photos.length - 1;
-    imgTag.src = photos[count];
-  } else {
-    imgTag.src = photos[count];
-  }
+function removeStyle() {
+  myVar.classList.remove("para-style");
 }
